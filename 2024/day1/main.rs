28eec1cc19f613569d26a1be1ac0a0ef2dc2d3b3
namiyousef <namiyousef@hotmail.com>
2024-12-07 07:52:19 +0000

    day1 pt2 adds env var to determine which part to run

--- 2024/day1/main.rs
+++ 2024/day1/main.rs
@@ -1,9 +1,34 @@
 use std::fs;
 use std::iter::zip;
+use std::env;
+use std::collections::HashMap;
+use std::collections::HashSet;
 
 fn main() {
 
-    let file_contents = fs::read_to_string("input.txt").expect(
+    match env::var("PART") {
+        Ok(val) => {
+            match val.as_str().parse().expect("Could not parse environment variable as an integer") {
+                1 => {
+                    println!("Starting part 1...")
+                },
+                2 => {
+                    println!("Starting part 2...");
+                    // create hashmap for storing the index of right addresses
+                    let mut right_addresses_index: HashMap<i32, i32> = HashMap::new();
+                    let mut right_addresses_counter: Vec<i32> = Vec::new();
+                },
+                _ => {
+                    println!("Got PATH of '{val}'. Expected '1' or '2'")
+                }
+            }
+        },
+        Err(_e) => {
+            panic!("Could not find the environment variable PART: Please set it to either '1' or '2'")
+        }
+    }
+
+    let file_contents = fs::read_to_string("assets/input.txt").expect(
         "Was not able to read the file"
     );
 
@@ -12,17 +37,20 @@ fn main() {
     let mut left_addresses: Vec<i32> = Vec::new();
     let mut right_addresses: Vec<i32> = Vec::new();
 
-    for address in addresses {
+    for (i, address) in addresses.enumerate() {
         let addresses_split: Vec<&str> = address.split("   ").collect();
 
         let left: &str = &addresses_split[0];
         let right: &str = &addresses_split[1];
 
+        let left_parsed: i32 = left.parse().expect("Could not read {left}");
+        let right_parsed: i32 = right.parse().expect("Could not read {right}");
+
         left_addresses.push(
-            left.parse().expect("Could not read {left}")
+            left_parsed
         );
         right_addresses.push(
-            right.parse().expect("Could not read {right}")
+            right_parsed
         )
     }
 
