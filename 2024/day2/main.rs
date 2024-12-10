6d50fd26626fa35c967e0f76e74becf98a699865
namiyousef <namiyousef@hotmail.com>
2024-12-10 18:26:29 +0000

    adds working code for part 2 day2

--- 2024/day2/main.rs
+++ 2024/day2/main.rs
@@ -19,7 +19,7 @@ fn main () {
             .collect()
         );
 
-        if is_report_safe(report) {
+        if is_report_safe(report, true) {
             num_safe_reports += 1;
         }
 
@@ -31,17 +31,41 @@ fn main () {
 
 }
 
+// function to create a vector but to ignore a specified index
+fn create_new_vector(report: Vec<i32>, index: usize) -> Vec<i32> {
+    let mut new_report: Vec<i32> = Vec::new();
+    for i in 0..report.len() {
+        if index != i {
+            new_report.push(report[i])
+        }
+    }
+
+    return new_report
+}
 /// Determines if a report is safe
-fn is_report_safe (report: Vec<i32>) -> bool {
+fn is_report_safe (report: Vec<i32>, tolerate_single_bad_level: bool) -> bool {
     let mut increasing = report[1] - report[0] > 0;
     for index in 0..report.len() {
         if index != 0 {
             let diff = report[index] - report[index - 1];
-            if (diff > 0) != increasing || diff == 0 {
-                return false
-            }
+            if (diff > 0) != increasing || diff == 0 || diff.abs() > 3 {
 
-            if diff.abs() > 3 {
+                if tolerate_single_bad_level {
+                    // brute force approach: search for all possible combinations, if one is safe then return true
+                    for index_2 in 0..report.len() {
+                        println!("Running for {index_2}. Report: {:?}", report);
+                        let new_report = create_new_vector(
+                            report.clone(), index_2
+                        );
+
+                        println!("Running for index {index_2}, modified report: {:?}", new_report);
+                        if is_report_safe(new_report, false) {
+                            return true
+                        }
+                    }
+                }
+                
+
                 return false
             }
         }
