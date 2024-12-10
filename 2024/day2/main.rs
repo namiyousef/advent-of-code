99b3f8ae123d1bcb96e3c25639f863c9debafbe3
namiyousef <namiyousef@hotmail.com>
2024-12-10 18:27:43 +0000

    simplifies code for day2 pt2

--- 2024/day2/main.rs
+++ 2024/day2/main.rs
@@ -31,17 +31,6 @@ fn main () {
 
 }
 
-// function to create a vector but to ignore a specified index
-fn create_new_vector(report: Vec<i32>, index: usize) -> Vec<i32> {
-    let mut new_report: Vec<i32> = Vec::new();
-    for i in 0..report.len() {
-        if index != i {
-            new_report.push(report[i])
-        }
-    }
-
-    return new_report
-}
 /// Determines if a report is safe
 fn is_report_safe (report: Vec<i32>, tolerate_single_bad_level: bool) -> bool {
     let mut increasing = report[1] - report[0] > 0;
@@ -54,11 +43,9 @@ fn is_report_safe (report: Vec<i32>, tolerate_single_bad_level: bool) -> bool {
                     // brute force approach: search for all possible combinations, if one is safe then return true
                     for index_2 in 0..report.len() {
                         println!("Running for {index_2}. Report: {:?}", report);
-                        let new_report = create_new_vector(
-                            report.clone(), index_2
-                        );
+                        let mut new_report = report.clone();
+                        new_report.remove(index_2);
 
-                        println!("Running for index {index_2}, modified report: {:?}", new_report);
                         if is_report_safe(new_report, false) {
                             return true
                         }
